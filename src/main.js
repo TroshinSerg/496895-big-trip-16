6bcdfac086924126a05a31c1076aa45e20c3e22c
Troshin Sergey <serge-troshin@yandex.ru>
2021-12-01 12:49:36 +0300

    Генерерует тестовые данные точек маршрута

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import {createEventsListTemplate} from './view/events-list-view.js';
 import {createPointTemplate} from './view/point-view.js';
 import {createEditPointTemplate} from './view/edit-point-view.js';
 import {createAddPointTemplate} from './view/add-point-view.js';
+import {generatePoint, DESTINATION_COUNT} from './mock/point.js';
 
 const tripMainElement = document.querySelector('.trip-main');
 const tripEventsElement = document.querySelector('.trip-events');
@@ -13,6 +14,26 @@ const navigationElement = tripMainElement.querySelector('.trip-controls__navigat
 const filtersElement = tripMainElement.querySelector('.trip-controls__filters');
 
 const EVENT_POINT_COUNT = 3;
+const POINTS_COUNT = 15;
+const points = [];
+
+const DestinationIdMap = (() => {
+  const part = Math.floor(POINTS_COUNT / DESTINATION_COUNT);
+  const map = {};
+
+  for (let i = 1; i <= DESTINATION_COUNT; i++) {
+    map[part * i] = i - 1;
+  }
+
+  return map;
+})();
+
+const destinationIdMapKeys = Object.keys(DestinationIdMap);
+
+for (let i = 1; i <= POINTS_COUNT; i++) {
+  const destinationId = DestinationIdMap[destinationIdMapKeys.find((it) => parseInt(it, 10) >= i)];
+  points.push(generatePoint(i, destinationId));
+}
 
 renderTemplate(navigationElement, createMenuTemplate(), RenderPosition.BEFOREEND);
 renderTemplate(filtersElement, createFiltersTemplate(), RenderPosition.BEFOREEND);
