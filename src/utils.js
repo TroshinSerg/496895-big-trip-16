d6e55da32232aeb8151440dbf2af0f4eb49cc47f
Troshin Sergey <serge-troshin@yandex.ru>
2021-12-01 12:42:31 +0300

    Исправляет код в соответствии с линтером

--- src/utils.js
+++ src/utils.js
@@ -3,8 +3,8 @@ const getRandomInteger = (a = 0, b = 1) => {
   const upper = Math.floor(Math.max(a, b));
 
   return Math.floor(lower + Math.random() * (upper - lower + 1));
-}
+};
 
 export {
   getRandomInteger
-}
+};
