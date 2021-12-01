5753c52e2ca79b963fe36b90db3329d6f4197de7
Troshin Sergey <serge-troshin@yandex.ru>
2021-12-01 20:39:01 +0300

    Дорабатывает функцию генерации дополнительных данных

--- src/mock/offer.js
+++ src/mock/offer.js
@@ -8,39 +8,39 @@ const OfferPrice = {
 const offerTitles = [
   {
     type: 'taxi',
-    titles: ['Order Uber', 'Add luggage', 'Upgrade to a comfort class', 'Upgrade to a business class', 'Upgrade to a premium class']
+    titles: [{title: 'Order Uber', mod: 'uber'}, {title: 'Add luggage', mod: 'luggage'}, {title: 'Upgrade to a comfort class', mod: 'comfort'}, {title: 'Upgrade to a business class', mod: 'business'}, {title: 'Upgrade to a premium class', mod: 'premium'}]
   },
   {
     type: 'bus',
-    titles: ['Add luggage', 'Switch to comfort', 'Choose seats', 'Add meal', 'Switch to comfort']
+    titles: [{title: 'Add luggage', mod: 'luggage'}, {title: 'Switch to comfort', mod: 'comfort'}, {title: 'Choose seats', mod: 'seats'}, {title: 'Add meal', mod: 'meal'}, {title: 'Switch to comfort', mod: 'comfort'}]
   },
   {
     type: 'train',
-    titles: ['Upgrade to a reserved seat', 'Upgrade to a compartment', 'Upgrade to a sleeping', 'Add luggage', 'Add meal']
+    titles: [{title: 'Upgrade to a reserved seat', mod: 'reserved-seat'}, {title: 'Upgrade to a compartment', mod: 'compartment'}, {title: 'Upgrade to a sleeping', mod: 'sleeping'}, {title: 'Add luggage', mod: 'luggage'}, {title: 'Add meal', mod: 'meal'}]
   },
   {
     type: 'ship',
-    titles: ['Upgrade to a comfort class', 'Upgrade to a business class', 'Upgrade to a premium class', 'Add luggage','Add meal']
+    titles: [{title: 'Upgrade to a comfort class', mod: 'comfort'}, {title: 'Upgrade to a business class', mod: 'business'}, {title: 'Upgrade to a premium class', mod: 'premium'}, {title: 'Add luggage', mod: 'luggage'}, {title: 'Add meal', mod: 'meal'}]
   },
   {
     type: 'drive',
-    titles: ['Reant a moto', 'Rent a car', 'Rent a car with a driver', 'Minibus rental', 'Minibus rental with a driver']
+    titles: [{title: 'Reant a moto', mod: 'moto'}, {title: 'Rent a car', mod: 'car'}, {title: 'Rent a car with a driver', mod: 'car-with-driver'}, {title: 'Minibus rental', mod: 'minibus'}, {title: 'Minibus rental with a driver', mod: 'minibus-with-driver'}]
   },
   {
     type: 'flight',
-    titles: ['Add luggage', 'Add meal', 'Switch to comfort', 'Choose seats', 'Travel by train']
+    titles: [{title: 'Add luggage', mod: 'luggage'}, {title: 'Add meal', mod: 'meal'}, {title: 'Switch to comfort', mod: 'comfort'}, {title: 'Choose seats', mod: 'seats'}, {title: 'Travel by train', mod: 'train'}]
   },
   {
     type: 'check-in',
-    titles: ['Add breakfast', 'Add lunch', 'Add supper', 'Add coffee', 'Add tea']
+    titles: [{title: 'Add breakfast', mod: 'breakfast'}, {title: 'Add lunch', mod: 'lunch'}, {title: 'Add supper', mod: 'supper'}, {title: 'Add coffee', mod: 'coffee'}, {title: 'Add tea', mod: 'tea'}]
   },
   {
     type: 'sightseeing',
-    titles: ['Book tickets', 'Lunch in city', 'Personal guide', 'Auto-excursion', 'Photo session']
+    titles: [{title: 'Book tickets', mod: 'tickets'}, {title: 'Lunch in city', mod: 'lunch'}, {title: 'Personal guide', mod: 'guide'}, {title: 'Auto-excursion', mod: 'auto-excursion'}, {title: 'Photo session', mod: 'photo'}]
   },
   {
     type: 'restaurant',
-    titles: ['Add breakfast', 'Add lunch', 'Add supper', 'Choose seats', 'Live music']
+    titles: [{title: 'Add breakfast', mod: 'breakfast'}, {title: 'Add lunch', mod: 'lunch'}, {title: 'Add supper', mod: 'supper'}, {title: 'Choose seats', mod: 'seats'}, {title: 'Live music', mod: 'music'}]
   }
 ];
 
@@ -49,7 +49,8 @@ const generateOffers = (type) => {
 
   return titles.map((it, index) => ({
     id: index + 1,
-    title: it,
+    title: it.title,
+    mod: it.mod,
     price: getRandomInteger(OfferPrice.MIN, OfferPrice.MAX),
     isChecked: Boolean(getRandomInteger(0, 1))
   }));
