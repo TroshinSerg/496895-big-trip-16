0fbb477deec1383a8ace2ab43eb1c4284e5e33ea
Troshin Sergey <serge-troshin@yandex.ru>
2021-12-08 19:57:41 +0300

    Рефакторинг render функций

--- src/render.js
+++ src/render.js
@@ -9,7 +9,33 @@ const renderTemplate = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
+const renderElement = (container, element, place) => {
+  switch(place) {
+    case RenderPosition.BEFOREBEGIN:
+      container.before(element);
+      break;
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(element);
+      break;
+    case RenderPosition.AFTEREND:
+      container.after(element);
+      break;
+  }
+};
+
+const createElement = (template) => {
+  const newElement = document.createElement('div');
+  newElement.innerHTML = template;
+
+  return newElement.firstChild;
+}
+
 export {
   renderTemplate,
+  renderElement,
+  createElement,
   RenderPosition
 };
