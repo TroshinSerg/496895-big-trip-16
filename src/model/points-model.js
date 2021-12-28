a7a2769fbb05950dcfefb707c2d911ddb1b1b321
TroshinSerg <serge-troshin@yandex.ru>
2021-12-28 16:56:28 +0300

    Добавляет в модели методы добавления, обновления и удаления точки маршрута

--- src/model/points-model.js
+++ src/model/points-model.js
@@ -10,4 +10,19 @@ export default class PointsModel extends AbstractObservable {
   get points() {
     return this.#points;
   }
+
+  updatePoint = (updateType, update) => {
+    this.#points = this.#points.map((point) => point.id === update.id ? update : point);
+    this._notify(updateType, update);
+  }
+
+  addPoint = (updateType, update) => {
+    this.#points = [update, ...this.#points];
+    this._notify(updateType, update);
+  }
+
+  deletePoint = (updateType, update) => {
+    this.#points = this.#points.filter((point) => point.id !== update.id);
+    this._notify(updateType);
+  }
 }
