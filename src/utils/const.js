ab8b0ec8c8dacc63e55705c26d41897cf54b7134
TroshinSerg <serge-troshin@yandex.ru>
2022-01-02 18:32:03 +0300

    Добавляет перечисление типов фильтра

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,17 +1,30 @@
-export const SortType = {
+const SortType = {
   DEFAULT: 'default',
   PRICE: 'price',
   TIME: 'time'
 };
 
-export const UserAction = {
+const UserAction = {
   UPDATE_POINT: 'UPDATE_POINT',
   ADD_POINT: 'ADD_POINT',
   DELETE_POINT: 'DELETE_POINT'
 };
 
-export const UpdateType = {
+const UpdateType = {
   PATCH: 'PATCH',
   MINOR: 'MINOR',
   MAJOR: 'MAJOR'
+};
+
+const FilterType = {
+  EVERYTHING: 'everything',
+  PAST: 'past',
+  FUTURE: 'future'
+};
+
+export {
+  FilterType,
+  UpdateType,
+  UserAction,
+  SortType
 };
