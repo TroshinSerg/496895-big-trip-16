dec0215c0698e3d8d5f670eff9b579f427a61d35
TroshinSerg <serge-troshin@yandex.ru>
2022-01-15 17:28:38 +0300

    Дорабатывает компонент меню

--- src/view/menu-view.js
+++ src/view/menu-view.js
@@ -1,9 +1,12 @@
 import AbstractView from './abstract-view.js';
+import { MenuItem } from '../utils/const.js';
+
+const MODIFIER_CLASS = 'trip-tabs__btn--active';
 
 const createMenuTemplate = () => (
-  `<nav class="trip-controls__trip-tabs  trip-tabs">
-    <a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Table</a>
-    <a class="trip-tabs__btn" href="#">Stats</a>
+  `<nav class="trip-controls__trip-tabs trip-tabs">
+    <a class="trip-tabs__btn ${MODIFIER_CLASS}" data-id="${MenuItem.TABLE}" href="#">${MenuItem.TABLE}</a>
+    <a class="trip-tabs__btn" data-id="${MenuItem.STATS}" href="#">${MenuItem.STATS}</a>
   </nav>`
 );
 
@@ -11,4 +14,25 @@ export default class MenuView extends AbstractView {
   get template() {
     return createMenuTemplate();
   }
+
+  setOnMenuClick = (callback) => {
+    this._callback.menuClick = callback;
+    this.element.addEventListener('click', this.#onMenuClick);
+  }
+
+  setActiveMenuItem = (itemId) => {
+    const menuItems = this.element.querySelectorAll('.trip-tabs__btn');
+
+    menuItems.forEach((menuItem) => {
+      menuItem.classList[menuItem.dataset.id === itemId ? 'add' : 'remove'](MODIFIER_CLASS);
+    });
+  };
+
+  #onMenuClick = (evt) => {
+    evt.preventDefault();
+    const menuItemId = evt.target.dataset.id;
+
+    this.setActiveMenuItem(menuItemId);
+    this._callback.menuClick(menuItemId);
+  };
 }
