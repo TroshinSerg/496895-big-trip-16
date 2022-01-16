45e0b1f85e6ebd7332a621370e8e9ab5fc750dce
TroshinSerg <serge-troshin@yandex.ru>
2022-01-16 15:38:59 +0300

    Реализует паттерн Адаптер

--- src/api-service.js
+++ src/api-service.js
@@ -21,7 +21,7 @@ export default class ApiService {
     const response = await this.#load({
       url: `points/${point.id}`,
       method: Method.PUT,
-      body: JSON.stringify(point),
+      body: JSON.stringify(this.#adaptToServer(point)),
       headers: new Headers({'Content-Type': 'application/json'}),
     });
 
@@ -49,6 +49,22 @@ export default class ApiService {
     } catch (err) {
       ApiService.catchError(err);
     }
+  }
+
+  #adaptToServer = (point) => {
+    const adaptedPoint = {...point,
+      'base_price': point.basePrice,
+      'date_from': point.dateFrom instanceof Date ? point.dateFrom.toISOString() : null,
+      'date_to': point.dateTo instanceof Date ? point.dateTo.toISOString() : null,
+      'is_favorite': point.isFavorite
+    };
+
+    delete adaptedPoint.basePrice;
+    delete adaptedPoint.dateFrom;
+    delete adaptedPoint.dateTo;
+    delete adaptedPoint.isFavorite;
+
+    return adaptedPoint;
   }
 
   static parseResponse = (response) => response.json();
--- src/model/points-model.js
+++ src/model/points-model.js
@@ -2,6 +2,20 @@ import AbstractObservable from './abstract-observable.js';
 
 export default class PointsModel extends AbstractObservable {
   #points = [];
+  #apiService = null;
+
+  constructor(apiService) {
+    super();
+    this.#apiService = apiService;
+
+    this.#apiService.points.then((points) => {
+       console.log(points.map(this.#adaptToClient));
+      // Есть проблема: cтруктура объекта похожа, но некоторые ключи называются иначе,
+      // а ещё на сервере используется snake_case, а у нас camelCase.
+      // Можно, конечно, переписать часть нашего клиентского приложения, но зачем?
+      // Есть вариант получше - паттерн "Адаптер"
+    });
+  }
 
   set points(points) {
     this.#points = [...points];
@@ -24,5 +38,22 @@ export default class PointsModel extends AbstractObservable {
   deletePoint = (updateType, update) => {
     this.#points = this.#points.filter((point) => point.id !== update.id);
     this._notify(updateType);
+  }
+
+  #adaptToClient = (point) => {
+    const adaptedPoint = {...point,
+      dateFrom: point['date_from'] !== null ? new Date(point['date_from']) : point['date_from'],
+      dateTo: point['date_to'] !== null ? new Date(point['date_to']) : point['date_to'],
+      basePrice: point['base_price'],
+      isFavorite: point['is_favorite']
+    };
+
+    // Ненужные ключи мы удаляем
+    delete adaptedPoint['date_from'];
+    delete adaptedPoint['date_to'];
+    delete adaptedPoint['is_favorite'];
+    delete adaptedPoint['base_price'];
+
+    return adaptedPoint;
   }
 }
